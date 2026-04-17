4426299a41e49063bdf98cf921c1719114678186
Raif Harik <harik.raif@gmail.com>
2026-04-17 09:06:14 -0500

    again

--- infra/scripts/deploy/load-infra-app-config.sh
+++ infra/scripts/deploy/load-infra-app-config.sh
@@ -52,6 +52,5 @@ echo "$MERGED" | jq -r '
   "DOCKER_NODE_VERSION=\(.docker.nodeVersion)",
   "DOCKER_APP_WORKSPACE_PATH=\(.docker.appWorkspacePath // .docker.apiWorkspacePath)",
   "DOCKER_NODE_ENTRYPOINT=\(.docker.nodeEntrypoint)",
-  "DOCKER_WORKERS_JSON=\((.docker.workers // []) | @json | @sh)",
-  "DOCKER_EXTRA_BACKEND_PATHS=\((.docker.workers // []) | map(.workspacePath) | join(\" \") | @sh)"
+  "DOCKER_WORKERS_JSON=\((.docker.workers // []) | @json | @sh)"
 '
