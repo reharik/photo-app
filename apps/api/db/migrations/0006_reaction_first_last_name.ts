0debbb1e915cb1d263898a437ffe8bba09d98317
Raif Harik <harik.raif@gmail.com>
2026-06-08 17:24:54 -0500

    fix migration

--- apps/api/db/migrations/0006_reaction_first_last_name.ts
+++ apps/api/db/migrations/0006_reaction_first_last_name.ts
@@ -1,14 +1,14 @@
 import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.alterTable('reactions', (table) => {
+  await knex.schema.alterTable('reaction', (table) => {
     table.string('first_name').nullable();
     table.string('last_name').nullable();
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.alterTable('reactions', (table) => {
+  await knex.schema.alterTable('reaction', (table) => {
     table.dropColumn('first_name');
     table.dropColumn('last_name');
   });
