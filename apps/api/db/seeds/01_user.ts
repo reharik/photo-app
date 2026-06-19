b52b1eedb0386c8fcdaef59a1ca1554fed2c22cf
Raif Harik <harik.raif@gmail.com>
2026-06-19 15:25:24 -0500

    more dumb stuff

--- apps/api/db/seeds/01_user.ts
+++ apps/api/db/seeds/01_user.ts
@@ -1,6 +1,6 @@
 import type { Knex } from 'knex';
 
-import { ensureSeedUsers } from '../seedUsers';
+import { ensureSeedUsers } from '../seedUsers.js';
 
 export const seed = async (knex: Knex): Promise<void> => {
   await ensureSeedUsers(knex);
