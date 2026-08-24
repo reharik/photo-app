ea41d0555153cddb9ab664cd695a0afead5ba222
Raif Harik <harik.raif@gmail.com>
2026-08-24 10:12:31 -0500

    getting ioc scripts working

--- packages/context/media-core/src/services/writeServices/comments/addComment.ts
+++ packages/context/media-core/src/services/writeServices/comments/addComment.ts
@@ -48,25 +48,20 @@ type AddCommentDeps = {
   writeServices: WriteServices;
 };
 
-export const build__AddComment = ({
-  commentRepository,
-  userReadRepository,
-  validateOperationService,
-  writeServices,
-}: AddCommentDeps): AddComment => {
+export const build__AddComment = (deps: AddCommentDeps): AddComment => {
   return async (command: AddCommentCommand): Promise<OperationResult<{ entityId: EntityId }>> => {
-    const result = await validateOperationService.authorizeMediaComment({
+    const result = await deps.validateOperationService.authorizeMediaComment({
       mediaItemId: command.targetId,
     });
     if (!result.success) {
       return result;
     }
-    const user = await userReadRepository.getById(command.authorId);
+    const user = await deps.userReadRepository.getById(command.authorId);
     if (!user) {
       return fail(AppErrorCollection.user.UserNotFound);
     }
     if (command.parentCommentId) {
-      const parentComment = await commentRepository.getById(command.parentCommentId);
+      const parentComment = await deps.commentRepository.getById(command.parentCommentId);
       if (!parentComment) {
         return fail(AppErrorCollection.comment.CommentNotFound);
       }
@@ -93,8 +88,8 @@ export const build__AddComment = ({
       command.authorId,
     );
 
-    await commentRepository.save(comment);
-    await writeServices.toggleReaction({
+    await deps.commentRepository.save(comment);
+    await deps.writeServices.toggleReaction({
       targetType: EntityType.mediaItem,
       targetId: command.targetId,
       emoji: ReactionEmoji.comment,
